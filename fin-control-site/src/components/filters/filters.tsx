49d26c77e839cbd15e9709d438b7adc64995f335
Anastasia <dzheyn.anna@mail.ru>
2024-09-15 23:47:28 +0400

    feat: добавляет центральную секцию wip#2

--- fin-control-site/src/components/filters/filters.tsx
+++ fin-control-site/src/components/filters/filters.tsx
@@ -3,31 +3,34 @@ import { ReadFilled, UploadOutlined } from "@ant-design/icons";
 import { CategoryName } from "@utils/constant";
 
 function Filters(): JSX.Element {
+    const date = '01.04.2023';
     return (
         <div className='filters'>
             <div className="filters__heading">
-                <h1 className="filters__title"></h1>
+                <h1 className="filters__title"> Остатки сформированы на {date} г.</h1>
                 <Button type='primary' icon={<ReadFilled />} className="filters__info-btn">Инструкции</Button>
             </div>
             <div className="filters__container">
                 <Form className="filters__form filters-form" autoComplete="off">
                     <Form.Item label="Баркод" name='barcode' className="filters-form__item">
-                        <InputNumber placeholder="5643242134323099" />
+                        <InputNumber controls={false} placeholder="5643242134323099" className="custom-input input-barcode"/>
                     </Form.Item>
                     <Form.Item label="Артикул" name='part_number' className="filters-form__item">
-                        <Input placeholder="ДжЖСинМом0823" />
+                        <Input placeholder="ДжЖСинМом0823" className="custom-input input-part_number" />
                     </Form.Item>
                     <Form.Item label="Размер" name='size' className="filters-form__item">
-                        <InputNumber placeholder="44" />
+                        <Input placeholder="44" className="custom-input input-size"/>
                     </Form.Item>
                     <Form.Item label="Категория" name='category' className="filters-form__item">
-                        <Select  defaultValue={CategoryName.jeans}>
+                        <Select  defaultValue={CategoryName.jeans} className="custom-select">
                             {Object.entries(CategoryName).map(([key, value]) => <Select.Option key={key} value={key}>{value}</Select.Option>)}
                         </Select>
                     </Form.Item>
                 </Form>
-                <Button type="primary" className="filters__submit-btn">Сформировать</Button>
-                <Button type="primary" className="filters__export-btn"  icon={<UploadOutlined /> }>Экспорт</Button>
+                <div className="filters__controls filter-controls">
+                <Button type="primary" className="filter-controls__submit">Сформировать</Button>
+                <Button type="primary" className="filter-controls__export"  icon={<UploadOutlined /> }>Экспорт</Button>
+            </div>
             </div>
         </div>
 
